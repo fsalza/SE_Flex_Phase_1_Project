588abab09f021bac4ec197ea915c156b2b58f8a2
Fulvio Salza <fsalza96@gmail.com>
2023-05-24 11:11:45 -0400

    create renderAllTeams and renderTeam

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ function fetchTeams() {
     .then(resp => resp.json())
     .then(teams => {
         console.log(teams)
-        teamCollection.innerHTML = renderTeams(teams)
+        teamCollection.innerHTML = renderAllTeams(teams)
     })
 }
 
 
+function renderAllTeams(allTeams) {
+    return allTeams.map(team => renderTeam(team)).join("")
+}
+
+function renderTeam(team){
+    return `
+    <div class="team-card" id="${team.id}">
+        <div class="team-card-frame">
+            <img src="${team.logo}" class="team-logo" />
+            <h1>${team.full_name}</h1>
+            <h2>${team.abbreviation}</h2>
+            <h2>${team.titles}</h2>
+            <button class="delete-button">Like </button>
+        </div>
+
+    </div>
+    `
+}
